66121c6e9a523d81ecdd46f1a0c2aa2f22716075
Maxim Spiridonov <taijased@gmail.com>
2019-02-26 11:04:28 +0400

    Initial Commit

---
+++ MVVM-Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-Coordinator
+//
+//  Created by Maxim Spiridonov on 26/02/2019.
+//  Copyright © 2019 Maxim Spiridonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
